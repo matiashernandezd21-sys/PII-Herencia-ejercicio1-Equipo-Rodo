77432a2746ea05a54581b38201f1799edb38689a
OliverBillig <oliver.billig2007@gmail.com>
2026-09-21 09:06:04 -0300

    driver semipronta

---
+++ src/Program/Driver.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Ucu.Poo.RideShare
+{
+    public class Driver
+    {
+        public Car Car { get; set; }
+
+        public String Bio { get; set; }
+
+    }
+}
